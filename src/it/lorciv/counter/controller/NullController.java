30091eea9f7cf8ac92895b8a1157d63e466d643c
Lorenzo Civolani <32967282+lcivolani@users.noreply.github.com>
2018-04-17 00:00:30 +0200

    Update NullController.java
    Improved log string

--- src/it/lorciv/counter/controller/NullController.java
+++ src/it/lorciv/counter/controller/NullController.java
@@ -4,22 +4,22 @@ public class NullController implements Controller {
 
 	@Override
 	public void handleIncrement() {
-		System.out.println("Controller: handle increment (do nothing)");
+		System.out.println("Controller: handle increment (doing nothing)");
 	}
 
 	@Override
 	public void handleReset() {
-		System.out.println("Controller: handle reset (do nothing)");
+		System.out.println("Controller: handle reset (doing nothing)");
 	}
 
 	@Override
 	public void handleUndo() {
-		System.out.println("Controller: handle undo (do nothing)");
+		System.out.println("Controller: handle undo (doing nothing)");
 	}
 
 	@Override
 	public void handleRedo() {
-		System.out.println("Controller: handle redo (do nothing)");
+		System.out.println("Controller: handle redo (doing nothing)");
 	}
 
 }
